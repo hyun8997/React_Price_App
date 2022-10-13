8b2e7633c6c4a1fb448d2acdfbfca4b6b3faa595
김경현 <yuiop999@ejbt.co.kr>
2022-10-13 10:55:19 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import ExpenseItem from "./components/ExpenseItem"; // 컴포넌트를 html처럼 사용 가능, 이름을 대문자부터 시작하도록 해야 함
+
+function App() {
+  //Props 사용해서 데이터 전달
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 28),
+    },
+  ];
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,30 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+// props 받음! key와 value 데이터를 받아옴
+function ExpenseItem(props) {
+  //하나의 컴포넌트는 하나로 묶는 부모 요소가 필수(root처럼)
+  //JSX의 html에는 class 대신 className을 사용함
+
+  return (
+    <div className="expense-item">
+      {
+        // 너무 커져서 다른 컴포넌트로 분리하려 함, 달력 형태 컴포넌트(ExpenseDate.js), 상단에 import 필요!
+        /* 
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div> 
+      */
+      }
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
