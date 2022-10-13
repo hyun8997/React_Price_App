a551681f32cad534f07f3264c74a80a93ff6702e
김경현 <yuiop999@ejbt.co.kr>
2022-10-13 11:18:34 +0900

    Expreses에 props 전달하는 방식으로 변경

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import Expenses from "./components/Expenses"; // 컴포넌트를 html처럼 사용 가능, 이름을 대문자부터 시작하도록 해야 함
 
 function App() {
+  //Props 사용해서 데이터 전달
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 28),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,29 +1,8 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
-function Expenses() {
-  //Props 사용해서 데이터 전달
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 28),
-    },
-  ];
+function Expenses(props) {
+  const expenses = props.expenses;
 
   return (
     <div className="expenses">
