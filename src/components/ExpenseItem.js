b74e7cf9d757f13e07cba0914b1b432b8ae8872a
김경현 <yuiop999@ejbt.co.kr>
2022-10-13 14:21:45 +0900

    래퍼 컴포넌트 Card 생성

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css";
 
 // props 받음! key와 value 데이터를 받아옴
@@ -7,7 +8,10 @@ function ExpenseItem(props) {
   //JSX의 html에는 class 대신 className을 사용함
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
+      {
+        // 기존의 div를 Card.js 래퍼 컴포넌트선언해서 코드 중복 제거
+      }
       {
         // 너무 커져서 다른 컴포넌트로 분리하려 함, 달력 형태 컴포넌트(ExpenseDate.js), 상단에 import 필요!
         /* 
@@ -23,7 +27,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
